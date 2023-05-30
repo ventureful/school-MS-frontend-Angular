9283f468b079cd56b59e904db6acf9a330047b16
ventureful <hades0122@outlook.com>
2023-05-30 04:27:32 -0400

    added app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TutorialListComponent } from './components/tutorial-list/tutorial-list.component';
 import { TutorialAddComponent } from './components/tutorial-add/tutorial-add.component';
+import { TutorialDetailComponent } from './components/tutorial-detail/tutorial-detail.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, TutorialListComponent, TutorialAddComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    TutorialListComponent,
+    TutorialAddComponent,
+    TutorialDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
